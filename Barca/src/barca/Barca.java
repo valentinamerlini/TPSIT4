902d52576ed6a2b8bc878ee807917e64bfb4656d
studente <studente@mat10>
2017-04-06 11:20:12 +0200

    Finchè la barca va

---
+++ Barca/src/barca/Barca.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package barca;
+
+/**
+ *
+ * @author studente
+ */
+public class Barca {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Finestra f=new Finestra();
+        f.setVisible(true);
+        f.setTitle("Finchè la barca va lasciala andare");
+    }
+    
+}
